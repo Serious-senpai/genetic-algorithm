92b7007a74e74709e1362625f29ea3d5ec64aa5d
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2023-12-23 23:25:18 +0700

    Print more details when building binary files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,10 @@
+g++ --version
+
 params="-O3 -Wall -shared -std=c++17 -fPIC $(python3-config --includes) -I extern/pybind11/include"
 extension=$(python3-config --extension-suffix)
 
 g++ $params ga/utils/cpp_utils.cpp -o ga/utils/cpp_utils$extension
+echo "Built ga/utils/cpp_utils$extension"
+
 g++ $params ga/vrpdfd/utils/cpp_utils.cpp -o ga/vrpdfd/utils/cpp_utils$extension
+echo "Built ga/vrpdfd/utils/cpp_utils$extension"
