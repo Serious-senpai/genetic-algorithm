e025c78f67c3fd8038a86f6ce4f3f92e465d0bbd
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-04-08 08:49:18 +0700

    Allow fast result comparison

--- scripts/vrpdfd-summary.py
+++ scripts/vrpdfd-summary.py
@@ -1,7 +1,8 @@
+import csv
 import json
 import os
 from pathlib import Path
-from typing import Any
+from typing import Any, Dict, List, Tuple
 
 from ga.vrpdfd import SolutionJSON
 
@@ -10,7 +11,31 @@ def wrap_double_quotes(text: Any) -> str:
     return f"\"{text}\""
 
 
+def sdvrp_results() -> Dict[str, Tuple[float, float]]:
+    sd_results: Dict[str, List[float]] = {}
+    with open("problems/vrpdfd/results/sdvrp-summary.csv", "r", encoding="utf-8", newline="") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",", quotechar="\"")
+        csvlines = iter(csvreader)
+        next(csvlines)  # Skip header
+        for csvline in csvlines:
+            problem = csvline[0].removesuffix(".txt")
+            result = float(csvline[6])
+
+            try:
+                sd_results[problem].append(result)
+            except KeyError:
+                sd_results[problem] = [result]
+
+
+    sd_summary: Dict[str, Tuple[float, float]] = {}
+    for problem, sd_result in sd_results.items():
+        sd_summary[problem] = (sum(sd_result) / len(sd_result), max(sd_result))
+
+    return sd_summary
+
+
 summary_dir = Path("vrpdfd-summary")
+summary_dir.mkdir(parents=True, exist_ok=True)
 field_names = (
     "Problem",
     "Generations",
@@ -27,19 +52,20 @@ field_names = (
     "Fake TSP solver",
     "Last improved",
     "Extra",
-    "I hit/miss/cached",
-    "T hit/miss/cached",
+    "SDVRP average",
+    "SDVRP max",
+    "Improved to avg [%]",
+    "Improved to max [%]",
 )
-
-
-summary_dir.mkdir(parents=True, exist_ok=True)
+sd_summary = sdvrp_results()
 with open(summary_dir / "vrpdfd-summary.csv", "w") as csvfile:
     csvfile.write(",".join(field_names) + "\n")
-    for file in sorted(os.listdir(summary_dir)):
+    for index, file in enumerate(sorted(os.listdir(summary_dir))):
         if file.startswith("output-") and file.endswith(".json"):
             with open(summary_dir / file, "r", encoding="utf-8") as f:
                 data: SolutionJSON = json.load(f)
 
+            sd_avg, sd_max = sd_summary[data["problem"]]
             fields = [
                 data["problem"],
                 data["generations"],
@@ -56,26 +82,10 @@ with open(summary_dir / "vrpdfd-summary.csv", "w") as csvfile:
                 int(data["fake_tsp_solver"]),
                 data["last_improved"],
                 data["extra"],
-                "/".join(
-                    map(
-                        str,
-                        [
-                            data["cache_info"]["individual"]["hit"],
-                            data["cache_info"]["individual"]["miss"],
-                            data["cache_info"]["individual"]["cached"],
-                        ],
-                    ),
-                ),
-                "/".join(
-                    map(
-                        str,
-                        [
-                            data["cache_info"]["tsp"]["hit"],
-                            data["cache_info"]["tsp"]["miss"],
-                            data["cache_info"]["tsp"]["cached"],
-                        ],
-                    ),
-                ),
+                sd_avg,
+                sd_max,
+                wrap_double_quotes(f"=ROUND(100 * (H{index + 2} - P{index + 2}) / ABS(P{index + 2}), 4)"),
+                wrap_double_quotes(f"=ROUND(100 * (H{index + 2} - Q{index + 2}) / ABS(Q{index + 2}), 4)"),
             ]
 
             csvfile.write(",".join(map(str, fields)) + "\n")
