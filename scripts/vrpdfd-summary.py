6afa324c6c73ceb79499a3d4552128e69dc16e33
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-05-10 10:03:55 +0700

    Update SDVRP results

--- scripts/vrpdfd-summary.py
+++ scripts/vrpdfd-summary.py
@@ -2,7 +2,7 @@ import csv
 import json
 import os
 from pathlib import Path
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict
 
 from ga.vrpdfd import SolutionJSON
 
@@ -11,26 +11,19 @@ def wrap_double_quotes(text: Any) -> str:
     return f"\"{text}\""
 
 
-def sdvrp_results() -> Dict[str, Tuple[float, float, float]]:
-    sd_results: Dict[str, List[float]] = {}
+def sdvrp_results() -> Dict[str, int]:
+    sd_results: Dict[str, int] = {}
     with open("problems/vrpdfd/results/sdvrp-summary.csv", "r", encoding="utf-8", newline="") as csvfile:
         csvreader = csv.reader(csvfile, delimiter=",", quotechar="\"")
         csvlines = iter(csvreader)
         next(csvlines)  # Skip header
         for csvline in csvlines:
-            problem = csvline[0].removesuffix(".txt")
-            result = float(csvline[6])
+            problem = csvline[1].removesuffix(".txt")
+            result = int(csvline[2])
 
-            try:
-                sd_results[problem].append(result)
-            except KeyError:
-                sd_results[problem] = [result]
+            sd_results[problem] = result
 
-    sd_summary: Dict[str, Tuple[float, float, float]] = {}
-    for problem, sd_result in sd_results.items():
-        sd_summary[problem] = (sum(sd_result) / len(sd_result), max(sd_result), len(sd_results))
-
-    return sd_summary
+    return sd_results
 
 
 summary_dir = Path("vrpdfd-summary")
@@ -51,50 +44,46 @@ field_names = (
     "Fake TSP solver",
     "Last improved",
     "Extra",
-    "SDVRP average",
     "SDVRP max",
-    "SDVRP run counts",
-    "Improved to avg [%]",
     "Improved to max [%]",
 )
 sd_summary = sdvrp_results()
 with open(summary_dir / "vrpdfd-summary.csv", "w") as csvfile:
     csvfile.write(",".join(field_names) + "\n")
-    for index, file in enumerate(sorted(os.listdir(summary_dir)), start=2):
-        if file.startswith("output-") and file.endswith(".json"):
-            with open(summary_dir / file, "r", encoding="utf-8") as f:
-                data: SolutionJSON = json.load(f)
+    for index, file in enumerate(
+        sorted(f for f in os.listdir(summary_dir) if f.startswith("output-") and f.endswith(".json")),
+        start=2,
+    ):
+        with open(summary_dir / file, "r", encoding="utf-8") as f:
+            data: SolutionJSON = json.load(f)
 
-            fields = [
-                data["problem"],
-                data["generations"],
-                data["population_size"],
-                data["mutation_rate"],
-                data["reset_after"],
-                data["stuck_penalty_increase_rate"],
-                data["local_search_batch"],
-                data["solution"]["profit"],
-                int(data["solution"]["feasible"]),
-                wrap_double_quotes(data["solution"]["truck_paths"]),
-                wrap_double_quotes(data["solution"]["drone_paths"]),
-                data["time"],
-                int(data["fake_tsp_solver"]),
-                data["last_improved"],
-                data["extra"],
-            ]
+        fields = [
+            data["problem"],
+            data["generations"],
+            data["population_size"],
+            data["mutation_rate"],
+            data["reset_after"],
+            data["stuck_penalty_increase_rate"],
+            data["local_search_batch"],
+            data["solution"]["profit"],
+            int(data["solution"]["feasible"]),
+            wrap_double_quotes(data["solution"]["truck_paths"]),
+            wrap_double_quotes(data["solution"]["drone_paths"]),
+            data["time"],
+            int(data["fake_tsp_solver"]),
+            data["last_improved"],
+            data["extra"],
+        ]
 
-            try:
-                sd_avg, sd_max, sd_count = map(str, sd_summary[data["problem"]])
-                fields.extend(
-                    [
-                        sd_avg,
-                        sd_max,
-                        sd_count,
-                        wrap_double_quotes(f"=ROUND(100 * (H{index} - P{index}) / ABS(P{index}), 4)"),
-                        wrap_double_quotes(f"=ROUND(100 * (H{index} - Q{index}) / ABS(Q{index}), 4)"),
-                    ],
-                )
-            except KeyError:
-                pass
+        try:
+            sd_max = sd_summary[data["problem"]]
+            fields.extend(
+                [
+                    sd_max,
+                    wrap_double_quotes(f"=ROUND(100 * (H{index} - P{index}) / ABS(P{index}), 4)"),
+                ],
+            )
+        except KeyError:
+            pass
 
-            csvfile.write(",".join(map(str, fields)) + "\n")
+        csvfile.write(",".join(map(str, fields)) + "\n")
